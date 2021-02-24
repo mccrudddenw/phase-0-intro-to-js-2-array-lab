5f394a7e01d1452092302783d321a3bfe3a91278
mccruddenw <wyatt.mccrudden@gmail.com>
2021-02-24 13:27:20 -0800

    Completed Assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -15,6 +15,7 @@ describe('index.js', function () {
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
         destructivelyAppendCat('Ralph');
+        
 
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
       });
